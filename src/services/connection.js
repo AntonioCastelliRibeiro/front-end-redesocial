21694c46297674b741c8b06f0acf5b423d1aba86
AntonioCastelliRibeiro <antoniocastelliribeiro@hotmail.com>
2020-10-05 22:17:23 -0300

    ip

--- src/services/connection.js
+++ src/services/connection.js
@@ -1,5 +1,8 @@
-const connection = 'http://192.168.15.6:3333'
+// const connection = 'http://192.168.15.6:3333'
 //  const connection = 'http://192.168.0.107:3333'
+
+const connection = process.env.REACT_APP_API_URL
+
 
 
 export default connection;
